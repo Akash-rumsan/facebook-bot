38ee2ca0c5b0c7b9b00c541a011471a65fdbc72d
Akash-rumsan <akash.sunar@rumsan.net>
2025-07-29 21:00:22 +0545

    code clean up 1

--- src/routes/setup.routes.ts
+++
@@ -1,9 +0,0 @@
-// routes/setup.routes.ts
-import express from "express";
-import { setupMessengerProfile } from "../controllers/setup.controller";
-
-const router = express.Router();
-
-router.post("/messenger-profile", setupMessengerProfile);
-
-export default router;
